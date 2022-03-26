cd5bc60d9d6fe12532213f60c7ad82bd3b75c0fe
SamuelOntiveros <enrissmuelo@gmail.com>
2022-03-26 04:33:38 -0300

    fix: some css redundant and take the scripts to he header

--- js/app.js
+++ js/app.js
@@ -1,26 +1,143 @@
-// typing
-const typed = new Typed(".typed", {
-    strings: ["¡Bienvenido!"],
-    // stringsElement: "#cadena-texto",
-    typeSpeed: 100,
-    backSpeed: 50,
-    smartBackspace: true,
-    backDelay: 3500,
-    loop: true,
-    loopCount: false,
-    showCursor: false,
-});
+// Aos
 
-//year
+AOS.init({
+    duration: 2000,
+});
+// typing
+// hero-styles
+
+// hero-styles
+const likes = [
+    "aprender nuevas cosas",
+    "resolver problemas",
+    "trabajar en equipo",
+    "desarrollar soluciones"
+];
+let i = 0;
+
+const like = () => {
+    if (i === likes.length - 1) {
+        i = 0;
+    } else {
+        i++;
+    }
+    document.getElementById("likes").innerHTML = likes[i];
+};
+like();
+
+setInterval(like, 1500);
+
+// Year
 const span = document.getElementById("year");
 const year = new Date();
 span.innerHTML = `${year.getFullYear()}`;
+
+// Menu Mobile
+const barra = document.getElementById("btnMenu");
+const menuSide = document.querySelector(".menu-side");
+const menuMobile = document.querySelector(".menu-mobile");
+
+barra.addEventListener("click", function() {
+    menuSide.classList.toggle("active");
+    menuMobile.classList.toggle("line");
+});
+
+const startMenu = document.getElementById("start-menu");
+const aboutMeMenu = document.getElementById("about-me-menu");
+const proyectsMenu = document.getElementById("proyects-menu");
+const habilityMenu = document.getElementById("hability-menu");
+const contactMenu = document.getElementById("contact-menu");
+
+function scrollDistance(e, element) {
+    e.preventDefault();
+    let hash = element.getAttribute("href");
+    let target = document.querySelector(hash);
+    let headerOffset = 100;
+    let elementPosition = target.offsetTop;
+    let offsetPosition = elementPosition - headerOffset;
+    window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth",
+    });
+}
+
+function scrollToReference(e, element) {
+    scrollDistance(e, element);
+    menuSide.classList.toggle("active");
+    menuMobile.classList.toggle("line");
+}
+
+startMenu.addEventListener("click", (e) => {
+    scrollToReference(e, startMenu);
+});
+aboutMeMenu.addEventListener("click", (e) => {
+    scrollToReference(e, aboutMeMenu);
+});
+proyectsMenu.addEventListener("click", (e) => {
+    scrollToReference(e, proyectsMenu);
+});
+habilityMenu.addEventListener("click", (e) => {
+    scrollToReference(e, habilityMenu);
+});
+contactMenu.addEventListener("click", (e) => {
+    scrollToReference(e, contactMenu);
+});
+
+//Scroll
+const start = document.querySelectorAll(".start");
+const aboutMe = document.querySelectorAll(".aboutMe");
+const proyects = document.querySelectorAll(".proyects");
+const hability = document.querySelectorAll(".hability");
+const contact = document.querySelectorAll(".contact");
+
+start.forEach((st) => {
+    st.addEventListener("click", (e) => {
+        scrollDistance(e, st);
+    });
+});
+
+aboutMe.forEach((st) => {
+    st.addEventListener("click", (e) => {
+        scrollDistance(e, st);
+    });
+});
+
+proyects.forEach((st) => {
+    st.addEventListener("click", (e) => {
+        scrollDistance(e, st);
+    });
+});
+
+hability.forEach((st) => {
+    st.addEventListener("click", (e) => {
+        scrollDistance(e, st);
+    });
+});
+
+contact.forEach((st) => {
+    st.addEventListener("click", (e) => {
+        scrollDistance(e, st);
+    });
+});
+
+// Fixed Header
+
+const fixedHeader = document.getElementById("fixed-header");
+
+window.addEventListener("scroll", () => {
+    let offsetHeader = 750;
+    if (window.scrollY >= offsetHeader) {
+        fixedHeader.style.marginTop = "0px";
+    } else if (window.scrollY <= offsetHeader) {
+        fixedHeader.style.marginTop = "-90px";
+    }
+});
 
 // Particles
 particlesJS({
     particles: {
         number: {
-            value: 80,
+            value: 100,
             density: {
                 enable: true,
                 value_area: 800,
@@ -45,7 +162,7 @@ particlesJS({
             },
         },
         opacity: {
-            value: 0.5,
+            value: 0.6,
             random: false,
             anim: {
                 enable: false,
@@ -66,14 +183,14 @@ particlesJS({
         },
         line_linked: {
             enable: true,
-            distance: 150,
+            distance: 160,
             color: "#fff",
-            opacity: 0.4,
+            opacity: 0.6,
             width: 1,
         },
         move: {
             enable: true,
-            speed: 6,
+            speed: 3,
             direction: "none",
             random: false,
             straight: false,
@@ -83,45 +200,6 @@ particlesJS({
                 enable: false,
                 rotateX: 600,
                 rotateY: 1200,
-            },
-        },
-    },
-    interactivity: {
-        detect_on: "canvas",
-        events: {
-            onhover: {
-                enable: true,
-                mode: "repulse",
-            },
-            onclick: {
-                enable: true,
-                mode: "push",
-            },
-            resize: true,
-        },
-        modes: {
-            grab: {
-                distance: 400,
-                line_linked: {
-                    opacity: 1,
-                },
-            },
-            bubble: {
-                distance: 400,
-                size: 40,
-                duration: 2,
-                opacity: 8,
-                speed: 3,
-            },
-            repulse: {
-                distance: 200,
-                duration: 0.4,
-            },
-            push: {
-                particles_nb: 4,
-            },
-            remove: {
-                particles_nb: 2,
             },
         },
     },
